5dd9b30c1c43f55f799867dc29ca4d8a8f9c0bdd
aduwaken <aduwa.kenny@gmail.com>
2026-06-17 14:14:20 +0300

    Add variables and data types practice

---
+++ index.js
@@ -0,0 +1,51 @@
+//Strings
+let firstName = "Kennedy";
+let lastName = "Aduwa";
+console.log(firstName);
+console.log(lastName);
+
+//Numbers
+let age = 25;
+let price = 99.99;
+console.log(age);
+console.log(price);
+
+//Booleans
+let isStudent = true;
+let isMarried = false;
+console.log(isStudent);
+console.log(isMarried);
+
+//Undefined
+let score;
+console.log(score);
+
+//Null
+let car = null;
+console.log(car);
+
+//Arrays
+let fruits = ["mango", "banana", "avocado"];
+console.log(fruits);
+
+//Objects
+let person = {
+    firstName: "Kennedy",
+    age: 25,
+    country: "Kenya"
+};
+console.log(person);
+
+//Combining  strings - old way
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
+//Combining strings - modern way (template literals)
+let greeting = `My name is ${firstName} ${lastName} and I am ${age} years old` ;
+console.log(greeting);
+
+
+
+
+
+ 
