256b82eec7c6c7376fd88bca00cac40de9393cb8
aduwaken <aduwa.kenny@gmail.com>
2026-06-17 19:45:43 +0300

    Add loops practice

--- index.js
+++ index.js
@@ -1,50 +1,26 @@
-function greet() {
-    console.log("Hello, Welcome to Javascript!");
+console.log("---Counting 1 to 10---");
+for (let i=1; i<=10; i++) {
+    console.log(i);
 }
 
-greet();
-greet();
-greet();
-
-function greetPerson(name) {
-console.log(`Hello, ${name}, welcome to Javascript!`)
+console.log("---Even numbers---");
+for (let i=2; i<=10; i+=2) {
+    console.log(i);
 }
 
-greetPerson("Kennedy");
-greetPerson("Brian");
-greetPerson("Alice");
-
-function addNumbers (a, b) {
-    return a+b;
+console.log("---While loop---");
+let count = 10;
+while (count>=1) {
+    console.log(count);
+    count--;
 }
+console.log("Blast off!");
 
-let result = addNumbers(10, 5);
-console.log("Sum:", result);
-console.log("Sum:", addNumbers(100, 200));
-
-function getGrade(score) {
-    if (score >= 90) {
-        return "A";
-    } else if (score >= 80) {
-        return "B";
-    } else if (score >= 70) {
-        return "C";
-    } else {
-        return "F";
-    }
-    }
-    
-    console.log("Grade for 95:", getGrade(95));
-    console.log("Grade for 82:", getGrade(82));
-    console.log("Grade for 60:", getGrade(60));
-
-    function calculateArea(width, height) {
-        return width*height;
-    }
-
-    let area = calculateArea(10, 5);
-    console.log("Area:", area);
-    console.log("Area:", calculateArea(200, 100));
+console.log("---Fruits---");
+let fruits = ["mango", "banana", "avocado", "orange"];
+for (let i=0; i<fruits.length; i++) {
+    console.log(`Fruit ${i+1}: ${fruits[i]}`);
+}
 
 
 
