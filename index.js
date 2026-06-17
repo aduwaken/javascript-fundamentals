f5b7d7f39225c57f8c033db343766a3404a1b47d
aduwaken <aduwa.kenny@gmail.com>
2026-06-17 20:29:51 +0300

    Add multiplication table and input validation

--- index.js
+++ index.js
@@ -1,27 +1,11 @@
-console.log("---Counting 1 to 10---");
-for (let i=1; i<=10; i++) {
-    console.log(i);
+function multiplicationTable(number) {
+    if (typeof number !== "number") {
+        console.log("Please provide a number!");
+        return;
+    }
+    for (let i = 1; i <= 10; i++) {
+        console.log(`${number} * ${i} = ${number * i}`);
+    }
 }
 
-console.log("---Even numbers---");
-for (let i=2; i<=10; i+=2) {
-    console.log(i);
-}
-
-console.log("---While loop---");
-let count = 10;
-while (count>=1) {
-    console.log(count);
-    count--;
-}
-console.log("Blast off!");
-
-console.log("---Fruits---");
-let fruits = ["mango", "banana", "avocado", "orange"];
-for (let i=0; i<fruits.length; i++) {
-    console.log(`Fruit ${i+1}: ${fruits[i]}`);
-}
-
-
-
- 
+multiplicationTable(7);
