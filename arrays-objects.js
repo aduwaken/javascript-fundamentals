e0b5b4b2cc7960cf4790cded0c98a7312fa9bf82
aduwaken <aduwa.kenny@gmail.com>
2026-06-21 13:39:51 +0300

    Add student filtering and grading challenge

--- arrays-objects.js
+++ arrays-objects.js
@@ -1,29 +1,42 @@
-let user = {
-    name: "kennedy",
-    skills: ["JavaScript", "Git", "HTML"],
-    address: {
-        city: "Nairobi",
-        country: "Kenya",
-    }
-};
-
-console.log(user.name);
-console.log(user.skills);
-console.log(user.skills[0]);
-console.log(user.address.city);
-console.log(user.address.country);
-
-let users = [
-    { name: "Kennedy", age: 25 },
-    { name: "Brian", age: 30 },
-    { name: "Alice", age: 28 }
+let students = [
+    { name: "Kennedy", score: 85 },
+    { name: "Brian", score: 45 },
+    { name: "Alice", score: 92 },
+    { name: "David", score: 60 },
+    { name: "Grace", score: 38 }
 ];
 
-console.log(users[0].name);
-console.log(users[1].age);
-console.log(users[2].name);
+let passed = students.filter(function(student) {
+    return student.score >= 50;
+});
+console.log("Passed students:", passed);
 
-users.forEach(function(user) {
-    console.log(`${user.name} is ${user.age} years old`)
+let studentNames = passed.map(function(pass) {
+    return pass.name;
+});
+console.log("Student Names:", studentNames);
+
+passed.forEach(function(students) {
+    console.log(`${students.name} passed with a score of ${students.score}`);
 });
 
+function getGrade(score) {
+    if (score >= 80) {
+        return "A";
+    } else if (score >= 70) {
+        return "B";
+    } else if (score >= 50) {
+        return "C";
+    } else {
+        return "F";
+    }
+}
+console.log(getGrade(85));
+console.log(getGrade(45));
+console.log(getGrade(92));
+console.log(getGrade(60));
+console.log(getGrade(38));
+
+students.forEach(function(grade) {
+    console.log(grade.name,":", "Grade", getGrade(grade.score));
+});
